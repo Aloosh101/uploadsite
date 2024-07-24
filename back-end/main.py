fe1b6b57c233b7fe30c65634282da70875237163
Aloosh101 <awsa09043@gmail.com>
2024-07-25 01:27:33 +0300

    update ...

--- back-end/main.py
+++ back-end/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fun import AESCipher, FileMerger, TeleSql
 from pydantic import BaseModel
 from io import BytesIO
-from threading import Lock
 
 app = FastAPI()
 
@@ -29,24 +28,20 @@ class MyData(BaseModel):
 datalist = {} 
 IDtele = {}
 
-lock = Lock()  # Lock for thread safety
-
-
 @app.post('/api/data')
 async def handle_data(mydata: MyData):
     print(f"Received Data: {mydata}")
 
-    with lock:  # Acquire lock before accessing shared data
-        id = mydata.id
-        data = mydata.data
-        message = mydata.message
+    id = mydata.id
+    data = mydata.data
+    message = mydata.message
 
-        if id not in datalist:
-            datalist[id] = []
+    if id not in datalist:
+        datalist[id] = []
 
-        datalist[id].append(data)
+    datalist[id].append(data)
 
-        if message == "done":
+    if message == "done":
             print(f"Data for ID {id} is done: {datalist[id]}")
             client = TeleSql(-1002070698546, session="session_name.session", api_id=25153583, api_hash="35543407ec1e319a3927f267183adb5d")
             client = await client.connect()
@@ -59,7 +54,7 @@ async def handle_data(mydata: MyData):
             token = AESCipher("token").decrypt_string(",".join(IDtele[id]))
             del IDtele[id]
             return token
-        elif len(datalist[id]) == 10:
+    elif len(datalist[id]) == 10:
             print(f"Data for ID {id} is full: {datalist[id]}")
             client = TeleSql(-1002070698546, session="session_name.session", api_id=25153583, api_hash="35543407ec1e319a3927f267183adb5d")
             client = await client.connect()
